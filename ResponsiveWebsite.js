a887478374beeefe4005f5d2330fe001fa290932
tringapps-Mariappan <123533770+Mariviknesh@users.noreply.github.com>
2023-03-13 15:48:50 +0530

    responsive

---
+++ ResponsiveWebsite.js
@@ -0,0 +1,10 @@
+let on=(srcs)=>{
+    document.getElementById("display").src=srcs;
+    document.getElementById("image").style.display="flex";
+
+}
+let off=()=>
+{
+    document.getElementById("image").style.display="none";
+
+}
